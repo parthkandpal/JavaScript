0f8e34f873e3f9dbdeb245dc96993c00fd59d340
Parth Kandpal <42544546+parthkandpal@users.noreply.github.com>
2018-11-06 11:26:10 +0530

    Update marvels.js

--- JSExerciseFiles/02 Basics/marvels.js
+++ JSExerciseFiles/02 Basics/marvels.js
@@ -7,3 +7,24 @@ console.log(superHeros[0]);
 console.log(superHeros[superHeros.length-1]);
 
 console.log(`We have ${superHeros.length} super heroes`);
+
+const numbers = ['one','two','three','four','five','six']
+
+//start
+console.log(numbers.shift())
+console.log(numbers)
+
+numbers.unshift('Something')
+console.log(numbers)
+
+//end
+numbers.pop()
+console.log(numbers)
+
+numbers.push('Seven')
+console.log(numbers)
+
+
+//middle
+numbers.splice(2, 2, 'something')
+console.log(numbers)
